1746b45ef736033e67395b4354673464a795301a
OleksandrPanichuk <oleksandr.panichuk.dev@gmail.com>
2024-07-25 10:43:36 +0300

    Dashboard header links were moved to a separate file

---
+++ src/layouts/DashboardLayout/components/Header/Header.data.ts
@@ -0,0 +1,33 @@
+import { Routes } from "@/shared/constants"
+
+export const headerContentMap: {
+	title: string
+	pathname: Routes
+	subtitle?: string
+}[] = [
+	{
+		pathname: Routes.PROFILE,
+		title: 'User Profile',
+		subtitle: 'Dashboard / Profile',
+	},
+	{
+		pathname: Routes.MY_LISTS,
+		title: 'My Wordlists',
+	},
+	{
+		pathname: Routes.BOOKMARKS,
+		title: 'Bookmarks',
+	},
+	{
+		pathname: Routes.BILLING,
+		title: 'Billing',
+	},
+	{
+		pathname: Routes.STATS,
+		title: 'Stats',
+	},
+	{
+		pathname: Routes.SETTINGS,
+		title: 'Settings',
+	},
+]
